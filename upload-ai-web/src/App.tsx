62a63446b0ce252edd54a35013ef7135690dee6f
Renan <renan.fachin@hotmail.com>
2023-09-12 16:29:40 -0300

    style: create textarea prompts area

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,6 +1,7 @@
 import { Button } from "./components/ui/button";
 import { Github } from 'lucide-react'
 import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
 
 export function App() {
   return (
@@ -28,7 +29,31 @@ export function App() {
       {/* Main */}
       {/* Flex-1 vai fazer ocupar o restante da altura limitado pela div pai */}
       <main className="flex-1 p-6 flex gap-6">
-        
+        <div className="flex flex-col flex-1 gap-4">
+          {/* textareas */}
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              // resize-none para não deixar o usuário modificar o tamanho
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+            />
+
+            <Textarea
+              className="resize-none p-5 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-500">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transicrição do video selecionado
+          </p>
+
+        </div>
+
+        <aside className="bg-blue-600 w-1/5">
+
+        </aside>
       </main>
     </div>
   )
