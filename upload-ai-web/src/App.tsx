e7622c3e1a1d07259ff828babfbf673fe15f4f39
Renan <renan.fachin@hotmail.com>
2023-09-12 16:21:00 -0300

    style: create header

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,7 +1,35 @@
 import { Button } from "./components/ui/button";
+import { Github } from 'lucide-react'
+import { Separator } from "./components/ui/separator";
 
 export function App() {
   return (
-    <Button>upload-ai</Button>
+    <div className="min-h-screen flex flex-col">
+      {/* Header */}
+      <header className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+
+        <div className="flex gap-3 items-center">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido no NLW da Rocketseat
+          </span>
+
+          {/* Por padrão ele vem na horizontal, mudando para vertical e definindo um tamanho */}
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </header>
+
+      {/* Main */}
+      {/* Flex-1 vai fazer ocupar o restante da altura limitado pela div pai */}
+      <main className="flex-1 p-6 flex gap-6">
+        
+      </main>
+    </div>
   )
 }
